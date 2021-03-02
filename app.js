30c078056f615a601857ed4926b3ad646f94a9fb
Roy-Arne Brussel <29131401+Roybrussel@users.noreply.github.com>
2021-03-02 19:24:32 +0100

    Login, logout, register and saving a session is now working via passport

--- app.js
+++ app.js
@@ -21,12 +21,11 @@ app.use(
 app.use(session({
   secret: "Our little secret.",
   resave: false,
-  saveUninitialized: false
+  saveUninitialized: false 
 }));
 
 app.use(passport.initialize());
 app.use(passport.session());
-
 
 mongoose.connect("mongodb://localhost:27017/userDB", {
   useNewUrlParser: true,
@@ -58,21 +57,55 @@ app.get("/login", function (req, res) {
 });
 
 app.get("/logout", function (req, res) {
-  res.render("login");
+  req.logout();
+  res.redirect("/");
 });
 
 app.get("/register", function (req, res) {
   res.render("register");
 });
 
+app.get("/secrets", function(req, res){
+  if (req.isAuthenticated()){
+    res.render("secrets");
+  } else {
+    res.redirect("/login");
+  }
+})
+
 app.post("/register", function (req, res) {
-  
+  User.register({username: req.body.username}, req.body.password, function (err, user){
+    if (err) {
+      console.log(err);
+      res.redirect("/register");
+    } else {
+      passport.authenticate("local")(req, res, function() {
+        res.redirect("/secrets");
+      });
+    }
+  })
 });
 
 app.post("/login", function (req, res) {
   
+  const user = new User({
+    username: req.body.username,
+    password: req.body.password,
+  });
+
+  req.login(user, function(err) {
+    if (err) {
+      console.log(err);
+      res.redirect("login");
+    } else {
+      passport.authenticate("local") (req, res, function() {
+        res.redirect("secrets");
+      })
+    }
+  })
+
 });
 
-app.listen(3000, function () {
-  console.log("Server started on port 3000.");
+app.listen(2311, function () {
+  console.log("Server started on port 2311.");
 });

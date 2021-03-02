24a348d8cdac9c2c191a17fe57fcbedebeb817ce
Roy-Arne Brussel <29131401+Roybrussel@users.noreply.github.com>
2021-03-02 19:55:35 +0100

    Adding a secret that is visible to other users as wel is working.

--- app.js
+++ app.js
@@ -37,6 +37,7 @@ mongoose.set("useCreateIndex", true)
 const userSchema = new mongoose.Schema({
   email: String,
   password: String,
+  secret: String,
 });
 
 userSchema.plugin(passportLocalMongoose);
@@ -66,12 +67,44 @@ app.get("/register", function (req, res) {
 });
 
 app.get("/secrets", function(req, res){
+  User.find({"secret": {$ne: null}}, function(err, foundUsers){
+    if (err){
+      console.log(err);
+    } else {
+      if (foundUsers) {
+        res.render("secrets", {usersWithSecrets: foundUsers});
+      }
+    }
+  });
+});
+
+app.get("/submit", function(req, res){
   if (req.isAuthenticated()){
-    res.render("secrets");
+    res.render("submit");
   } else {
     res.redirect("/login");
   }
-})
+});
+
+app.post("/submit", function(req, res){
+  const submittedSecret = req.body.secret;
+
+//Once the user is authenticated and their session gets saved, their user details are saved to req.user.
+  // console.log(req.user.id);
+
+  User.findById(req.user.id, function(err, foundUser){
+    if (err) {
+      console.log(err);
+    } else {
+      if (foundUser) {
+        foundUser.secret = submittedSecret;
+        foundUser.save(function(){
+          res.redirect("/secrets");
+        });
+      }
+    }
+  });
+});
 
 app.post("/register", function (req, res) {
   User.register({username: req.body.username}, req.body.password, function (err, user){
